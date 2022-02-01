834fceea399767998f92e3c83bbafd75ed6245bb
Obolduy <ivanox01@yandex.ru>
2022-02-01 11:48:22 +0300

    CurlController added

---
+++ app/Http/Controllers/CurlController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CurlController extends Controller
+{
+    public function sendCurlRequest(array $headers, string $url = 'https://brandshop.ru/sale/?limit=240'): string
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HTTPHEADER => $headers
+        ]);
+
+        return curl_exec($curl);
+    }
+}
