d54210e5e06135c858b3522c8c4689d3483d8a50
Obolduy <ivanox01@yandex.ru>
2022-02-02 15:57:12 +0300

    Archive model

---
+++ app/Models/Archive.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Archive extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'former_sale_id',
+        'added_at',
+        'archived_at',
+        'original_link_id',
+        'cutted_link_id'
+    ];
+
+    public $timestamps = false;
+}
