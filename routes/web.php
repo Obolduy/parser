2a9be961bbcdc9f54c926e6c5fc23f5d6c129162
Obolduy <ivanox01@yandex.ru>
2022-02-07 13:10:03 +0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\{LinkCutterLoginController, UserGetSalesController};
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
+Route::match(['POST', 'GET'], '/login', [LinkCutterLoginController::class, 'loginviaapi']);
+Route::get('/getsales', [UserGetSalesController::class, 'userGetSales']);
+Route::get('/getsales/table', [UserGetSalesController::class, 'userGetExcel']);
